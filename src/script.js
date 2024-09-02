cf6434c40ded807b8fd22ab7fde331818fbc0467
Takoyaki0805 <ilikemidori0805@gmail.com>
2024-09-02 23:44:37 +0900

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,10 @@
+let skill = document.getElementById("skill");
+let work = document.getElementById("work");
+
+function Moveskill(){
+    scrollTo(0,skill.offsetTop);
+}
+
+function Movework(){
+    scrollTo(0,work.offsetTop);
+}
