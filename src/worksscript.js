be6c9e5c59f12b4b77a41c3fc554dfe0cfceaab4
Takoyaki0805 <ilikemidori0805@gmail.com>
2024-10-18 00:06:25 +0900

    Add files via upload

--- src/worksscript.js
+++ src/worksscript.js
@@ -1,7 +1,33 @@
 function data(e){
-    let tag = e.currentTarget.id;
+    let tag
+    if(e == undefined){}
+    else{
+        tag = e.currentTarget.id;
+    }
+    if(tag==""){return}
     switch (tag){
         case "app1":
+            $("#name").html("支出表アプリ"); 
+            $("#img").attr("src","src/app1.png");
+            $("#img").attr("width","20%");
+            $("#img").attr("height","20%");
+            $("#img").attr("text-align","center");
+            $('#using').html("AndroidStudio,Java");
+            $('#what').html("AndroidStudioの練習と普段使いできるような、支出を素早くメモできるものがほしかったから"); 
+            $('#appeal').html("起動してすぐにメモができるUI");
+            $('#comment').html("初めて作ったアプリだったのでソースが雑になってしまった。<br>素早くかけるが、管理が面倒");      
+        break;
+        case "app2":
+            $("#name").html("ブラックジャック"); 
+            $("#img").attr("src","src/app2.png");
+            $("#img").attr("width","40%");
+            $("#img").attr("height","40%");
+            $('#using').html("Java(JFrame),Eclipse");
+            $('#what').html("ゲームエンジンに頼らずにゲームを作るを目標に制作"); 
+            $('#appeal').html("JFrameの要素を使って掛け金などを実装した。<br>クラスの概念に極力気を使った");
+            $('#comment').html("<a href='https://github.com/Takoyaki0805/BlackJack'>ソースコードはこちらから見られます。</a>");      
+        break;
+        default:
             $("#name").html("支出表アプリ"); 
             $("#img").attr("src","src/app1.png");
             $("#img").attr("width","20%");
@@ -12,9 +38,12 @@ function data(e){
             $('#comment').html("初めて作ったアプリだったのでソースが雑になってしまった。<br>素早くかけるが、管理が面倒");      
         break;
     }
-    // console.log(e.currentTarget.id);
+    // console.log(tag);
 }
 
 
-$('#app1').on('click',data)
+$('li').on('click',data)
+// $().on('click',data)
 // $('#app1').ready(data)
+let e;
+data(e);
